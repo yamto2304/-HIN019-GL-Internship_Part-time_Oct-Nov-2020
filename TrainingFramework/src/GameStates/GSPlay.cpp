68cf3f46c655591d52a633745116b77fb2e68cc0
HoangTuan <yamto2304>
2020-10-30 05:47:12 +0700

    Lesson5 SampleFramework

---
+++ TrainingFramework/src/GameStates/GSPlay.cpp
@@ -0,0 +1,88 @@
+#include "GSPlay.h"
+
+#include "Shaders.h"
+#include "Texture.h"
+#include "Models.h"
+#include "Camera.h"
+#include "Font.h"
+#include "Sprite2D.h"
+#include "Sprite3D.h"
+#include "Text.h"
+
+extern int screenWidth; //need get on Graphic engine
+extern int screenHeight; //need get on Graphic engine
+
+GSPlay::GSPlay()
+{
+}
+
+
+GSPlay::~GSPlay()
+{
+
+}
+
+
+void GSPlay::Init()
+{
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("bg_play");
+
+	//BackGround
+	auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+	m_BackGround = std::make_shared<Sprite2D>(model, shader, texture);
+	m_BackGround->Set2DPosition(screenWidth / 2, screenHeight / 2);
+	m_BackGround->SetSize(screenWidth, screenHeight);
+
+
+	//text game title
+	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
+	std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("arialbd");
+	m_score = std::make_shared< Text>(shader, font, "score: 10", TEXT_COLOR::RED, 1.0);
+	m_score->Set2DPosition(Vector2(5, 25));
+}
+
+void GSPlay::Exit()
+{
+
+}
+
+
+void GSPlay::Pause()
+{
+
+}
+
+void GSPlay::Resume()
+{
+
+}
+
+
+void GSPlay::HandleEvents()
+{
+
+}
+
+void GSPlay::HandleKeyEvents(int key, bool bIsPressed)
+{
+	
+}
+
+void GSPlay::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+}
+
+void GSPlay::Update(float deltaTime)
+{
+}
+
+void GSPlay::Draw()
+{
+	m_BackGround->Draw();
+	m_score->Draw();
+}
+
+void GSPlay::SetNewPostionForBullet()
+{
+}
---
+++ TrainingFramework/src/GameStates/GSPlay.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "gamestatebase.h"
+
+class Sprite2D;
+class Sprite3D;
+class Text;
+
+class GSPlay :
+	public GameStateBase
+{
+public:
+	GSPlay();
+	~GSPlay();
+
+	void Init();
+	void Exit();
+
+	void Pause();
+	void Resume();
+
+	void HandleEvents();
+	void HandleKeyEvents(int key, bool bIsPressed);
+
+	void HandleTouchEvents(int x, int y, bool bIsPressed);
+	void Update(float deltaTime);
+	void Draw();
+
+	
+	void SetNewPostionForBullet();
+
+private:
+
+	std::shared_ptr<Sprite2D> m_BackGround;
+	std::shared_ptr<Text>  m_score;
+
+};
+
